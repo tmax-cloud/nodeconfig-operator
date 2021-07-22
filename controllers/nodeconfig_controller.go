1f77e961f4cf20e3dab5693321f353ba5629e6a2
eunseong <godpeople16@naver.com>
2021-07-22 01:22:30 +0000

    remove internal directory

--- controllers/nodeconfig_controller.go
+++ controllers/nodeconfig_controller.go
@@ -23,7 +23,7 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
-	"github.com/tmax-cloud/nodeconfig-operator/internal/cloudinit"
+	"github.com/tmax-cloud/nodeconfig-operator/cloudinit"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
