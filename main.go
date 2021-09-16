a0f60c0f495f909ef402450a6f0dc6120cf4b131
eunseong <eunseong_lee@tmax.co.kr>
2021-09-16 01:01:25 +0000

    [feat] add webhook validating a NodeConfig

--- main.go
+++ main.go
@@ -87,6 +87,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "NodeConfig")
 		os.Exit(1)
 	}
+	if err = (&bootstrapv1alpha1.NodeConfig{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "NodeConfig")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
